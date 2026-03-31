ca15599a618c995b1497a6eff7c7bf5f1b7068a4
junczhu-ui <junczhu@bu.edu>
2026-03-31 18:53:58 -0400

    Update SavedListPage.tsx

--- frontend/src/pages/student/SavedListPage.tsx
+++ frontend/src/pages/student/SavedListPage.tsx
@@ -26,7 +26,11 @@ export default function SavedListPage() {
 
   const toggle = (id: string) => {
     const next = new Set(selected)
-    next.has(id) ? next.delete(id) : next.add(id)
+    if (next.has(id)) {
+      next.delete(id)
+    } else {
+      next.add(id)
+    }
     setSelected(next)
   }
 
