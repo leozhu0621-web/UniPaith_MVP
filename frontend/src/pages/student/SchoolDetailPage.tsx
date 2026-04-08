b20bbc99b6fde1230e71d346d114a544101f2b59
junczhu-ui <junczhu@bu.edu>
2026-04-08 14:39:38 -0400

    Update SchoolDetailPage.tsx

--- frontend/src/pages/student/SchoolDetailPage.tsx
+++ frontend/src/pages/student/SchoolDetailPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate, Link } from 'react-router-dom'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { getProgram } from '../../api/programs'
 import { getMatchDetail, logEngagement } from '../../api/matching'
